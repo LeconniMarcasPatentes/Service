e943b35372c74250de89c0d6ee7e632c516a6b98
giuseppe cordeiro <128421824+giusfds@users.noreply.github.com>
2024-09-27 17:33:48 -0300

    eu simplesmente desisto disso

--- install.bat
+++ install.bat
@@ -3,6 +3,9 @@ REM Instalar dependências
 pip install pandas
 pip install pydrive
 pip install openpyxl
+pip install secure-smtplib
+pip install python-dotenv
 
 REM Executar o programa
+cls
 python main.py
